309a86c2683c5c429a41a4df7df462525646e29a
ctb116 <cathytbradley116@gmail.com>
2022-09-01 20:56:34 -0700

    good commit - toggle works

--- index.js
+++ index.js
@@ -5,44 +5,42 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+// Get the container elements
 const contentContainer = document.getElementById('content');
-
-// Get the container element
 var btnContainer = document.getElementById('navigation');
 
 // create number of buttons based on the number of div child elements
-for (var i = 0; i < contentContainer.childElementCount; i++) {
-  // contentContainer.querySelector(`:nth-child(${i})`).classList.add(i);
+for (var i = 1; i <= contentContainer.childElementCount; i++) {
+  contentContainer.querySelector(`:nth-child(${i})`).setAttribute('id', i);
+  contentContainer.querySelector(`:nth-child(${i})`).classList.add('cnts');
 
   const button = btnContainer.appendChild(document.createElement('button'));
   button.classList.add('btn');
-  button.innerHTML = i + 1;
+  button.innerHTML = i;
 }
 
+// assign the first div to display
+contentContainer.querySelector(':nth-child(1)').className += ' show';
 // assign the first button as active
 var btns = btnContainer.getElementsByClassName('btn');
 btns[0].className += ' active';
 
 // Loop through the buttons and add the active class to the current/clicked button
+// Uses the innerHTML of the button clicked to display the corresponding div
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', function () {
     var current = document.getElementsByClassName('active');
     current[0].className = current[0].className.replace(' active', '');
     this.className += ' active';
 
-    var currentChild = contentContainer.querySelector(`:nth-child(${i})`);
-    console.log(this.innerHTML);
+    var currentContent = document.getElementsByClassName('show');
+    console.log(currentContent[0]);
+    currentContent[0].className = currentContent[0].className.replace(
+      ' show',
+      ''
+    );
 
-    contentContainer.querySelector(
-      `:nth-child(${this.innerHTML})`
-    ).style.display = 'block';
-
-    if (currentChild != this.innerHTML) {
-      console.log(currentChild);
-      contentContainer.querySelector(`:nth-child(${i})`).style.display = 'none';
-    } else {
-      contentContainer.querySelector(`:nth-child(${i})`).style.display =
-        'block';
-    }
+    contentContainer.querySelector(`:nth-child(${this.innerHTML})`).className +=
+      ' show';
   });
 }
