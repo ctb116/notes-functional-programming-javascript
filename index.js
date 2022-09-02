51b751a07a65dc4ec1506f8af3eb7448369d1332
ctb116 <cathytbradley116@gmail.com>
2022-09-01 17:39:07 -0700

    3 files modified

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-console.log('test I have made a change');
-
 // ignore everything below this line
 // ************************************************
 import './style.css';
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const contentContainer = document.getElementById('content');
+
+console.log(contentContainer.childElementCount);
+
+// Get the container element
+var btnContainer = document.getElementById('navigation');
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName('btn');
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
+  });
+}
